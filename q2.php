4149d222126095bc4d1d0d0c15e06eb853bb7063
Sharon Mathew Sunny <sharonmathewsunny16@gmail.com>
2022-11-17 11:51:22 +0530

    Add files via upload

---
+++ q2.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <body>
+            <?php
+                $a = array(10, 10, 10, 10);
+
+                $sum = array_sum($a);
+
+                echo ("The sum is"."$sum");   
+            ?>
+        </body>
+    </head>        
+<html>
